623637cbd5a17c7b3b7dc59431adf5995e4f7f6a
xrago-afk <lorza2000@gmail.com>
2020-03-16 15:40:34 +0100

    Primi esempi di inerpolazione

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DemoBergamo';
+
+  getVal(){
+    return 6;
+  }
+
+  HTMLstring = "<div><p>Prova</p></div>"
 }
